dd015cbd1ad02091e7af1079ad4f5e9969c0206e
AbhinavSingh <singhabhinav9051571833@gmail.com>
2019-11-28 21:40:23 +0530

    s

--- rclpy/test/test_node.py
+++ rclpy/test/test_node.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pathli
+import pathlib
 import unittest
 from unittest.mock import Mock
 
