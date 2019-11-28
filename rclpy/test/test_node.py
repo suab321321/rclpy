d2adb17956d47b3ac784bc9296598bd9460be472
AbhinavSingh <singhabhinav9051571833@gmail.com>
2019-11-28 21:40:01 +0530

    asa

--- rclpy/test/test_node.py
+++ rclpy/test/test_node.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pathlib
+import pathli
 import unittest
 from unittest.mock import Mock
 
